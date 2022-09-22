02eaeb5f65a18efc6931e16fb33865d69fbfcf28
Barbara Jeziorek <barbarajeziorek@gmail.com>
2022-09-22 08:37:20 +0200

    minor fixes

--- JpZd/src/data/Grammar/JpGrammar/but/but.ts
+++ JpZd/src/data/Grammar/JpGrammar/but/but.ts
@@ -5,8 +5,8 @@ import rand from "../../../../utils/randomArrayElement";
 
 export function but(theme:string):DataType{
     const who=rand(pickTheme(theme))
-    const adj1=rand(adjectives)
-    const adj2=rand(adjectives)
+    const adj1=rand(adjectives).replace(/na$/,'')
+    const adj2=rand(adjectives).replace(/na$/,'')
     return{
         romaji:who.jp+'-wa '+adj1.jp+' desu-ga '+adj2.jp+' desu',
         meaning:who.pl.M+' jest '+adj1.pl+', ale '+adj2.pl
