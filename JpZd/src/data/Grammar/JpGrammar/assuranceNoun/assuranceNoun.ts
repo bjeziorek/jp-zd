ff3ed7d1ae110131f036e811964e505fc1be5a3b
Barbara Jeziorek <barbarajeziorek@gmail.com>
2022-09-24 08:18:06 +0200

    minor fixes

--- JpZd/src/data/Grammar/JpGrammar/assuranceNoun/assuranceNoun.ts
+++ JpZd/src/data/Grammar/JpGrammar/assuranceNoun/assuranceNoun.ts
@@ -5,7 +5,7 @@ import rand from '../../../../utils/randomArrayElement';
 export function assuranceNoun(theme:string):DataType{
     const noun=rand(pickTheme(theme))
     return {
-        romaji:noun.jp+' wazu desu',
+        romaji:noun.jp+'-no wazu desu',
         meaning:'Na pewno jest '+noun.pl.N
     }
 }
