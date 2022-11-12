285a6b51411de123cb07320e4c9ee98d289c9d7a
Barbara Jeziorek <barbarajeziorek@gmail.com>
2022-11-12 20:51:58 +0100

    JP-304-306 GUI

--- JpZd/src/components/GrammarList.tsx
+++ JpZd/src/components/GrammarList.tsx
@@ -1,13 +1,51 @@
-import { IonCheckbox, IonChip } from '@ionic/react'
-import React from 'react'
-import { nouns } from '../data/dictionary'
+import { IonAccordion, IonAccordionGroup, IonCheckbox, IonChip, IonItem, IonLabel } from "@ionic/react";
+import React, { FormEvent, FormEventHandler } from "react";
+import { nouns } from "../data/dictionary";
+import { getKeyList } from "../UaUtils/getKeyList";
 
 export default function GrammarList() {
-    
+
+    const checkBoxStates=[]
+  //  const [checkBoxes,setCheckBoxes]=setState([])
+
+    function changeCheckBoxState(event: FormEvent){
+        console.log('check box changed to: ',
+        ((event as FormEvent).target as HTMLIonCheckboxElement).parentElement?.children[1].firstChild?.nodeValue,
+        ((event as FormEvent).target as HTMLIonCheckboxElement).checked
+        
+        )
+    }
+
+  const a = getKeyList(nouns).map((k) => {
+    return (
+      <IonAccordionGroup>
+        <IonAccordion value="first">
+          <IonItem slot="header" color="light">
+            <IonLabel>Nouns: {k}</IonLabel>
+          </IonItem>
+          {nouns[k].map((v) => (
+            <div className="ion-padding" slot="content">
+              <IonCheckbox onClick={changeCheckBoxState} />
+              <span>{v.jp}</span>
+            </div>
+          ))}
+        </IonAccordion>
+      </IonAccordionGroup>
+    );
+  });
+  const b = nouns.animals.map((v) => (
+    <p>
+      <IonChip>
+        <IonCheckbox />
+        {v.jp}
+      </IonChip>
+    </p>
+  ));
+
   return (
     <div>
-        <span>GrammarList</span>
-    {nouns.animals.map(v=><p><IonChip><IonCheckbox />{v.jp}</IonChip></p>)}
+      <span>GrammarList</span>
+      {a}
     </div>
-  )
+  );
 }
