774fcf541d0fba5e41b3bf96bb08cbb1b844e145
Barbara Jeziorek <barbarajeziorek@gmail.com>
2022-11-13 13:59:12 +0100

    JP-307 gui

--- JpZd/src/components/GrammarList.tsx
+++ JpZd/src/components/GrammarList.tsx
@@ -1,51 +1,105 @@
-import { IonAccordion, IonAccordionGroup, IonCheckbox, IonChip, IonItem, IonLabel } from "@ionic/react";
-import React, { FormEvent, FormEventHandler } from "react";
+import {
+  IonAccordion,
+  IonAccordionGroup,
+  IonCheckbox,
+  IonItem,
+  IonLabel,
+} from "@ionic/react";
+import React, { FormEvent, useState } from "react";
 import { nouns } from "../data/dictionary";
-import { getKeyList } from "../UaUtils/getKeyList";
 
 export default function GrammarList() {
+  const [checkboxes, setCheckboxes] = useState(initCheckboxes);
+  console.log("checkboxes", checkboxes);
+  console.log("Object.keys(obj)", Object.keys(nouns));
 
-    const checkBoxStates=[]
-  //  const [checkBoxes,setCheckBoxes]=setState([])
+  interface I {
+    [key: string]:
+      | string
+      | boolean
+      | {
+          subName: string;
+          subValue: boolean;
+        }[];
+  }
 
-    function changeCheckBoxState(event: FormEvent){
-        console.log('check box changed to: ',
-        ((event as FormEvent).target as HTMLIonCheckboxElement).parentElement?.children[1].firstChild?.nodeValue,
-        ((event as FormEvent).target as HTMLIonCheckboxElement).checked
-        
-        )
-    }
+  function initCheckboxes(): Array<I> {
+    //jeśli nie ma zapisu w localstrorage tu null-uj:
+    return Object.keys(nouns).map((category) => {
+      return {
+        name: category,
+        value: false,
+        subvalues: nouns[category].map((sc) => {
+          return {
+            subName: sc.jp,
+            subValue: false,
+          };
+        }),
+      };
+    });
+  }
 
-  const a = getKeyList(nouns).map((k) => {
+  function changeCheckboxState(event: FormEvent) {
+    //const name: string = ((event as FormEvent).target as HTMLIonCheckboxElement)
+      //.parentElement?.children[1].firstChild?.nodeValue as string;
+    const data = ((event as FormEvent).target as HTMLIonCheckboxElement)
+      .dataset;
+    const t = checkboxes;
+    const category = data.category ? data.category : "animals";
+    const value = data.value ? data.value : "neko";
+    const result = (
+      t.filter((item) => item.name === category)[0].subvalues as Array<{
+        subName: string;
+        subValue: boolean;
+      }>
+    ).filter(
+      (item2: { subName: string; subValue: boolean }) => item2.subName === value
+    )[0].subValue;
+    (
+      t.filter((item) => item.name === category)[0].subvalues as Array<{
+        subName: string;
+        subValue: boolean;
+      }>
+    ).filter(
+      (item2: { subName: string; subValue: boolean }) => item2.subName === value
+    )[0].subValue = !result;
+    setCheckboxes(t);
+  }
+
+  const a = Object.keys(nouns).map((k) => {
     return (
       <IonAccordionGroup>
-        <IonAccordion value="first">
+        <IonAccordion value="first" key={k}>
           <IonItem slot="header" color="light">
+            <IonCheckbox />
             <IonLabel>Nouns: {k}</IonLabel>
           </IonItem>
-          {nouns[k].map((v) => (
-            <div className="ion-padding" slot="content">
-              <IonCheckbox onClick={changeCheckBoxState} />
-              <span>{v.jp}</span>
+          {nouns[k].map((v, i) => (
+            <div
+              className="ion-padding"
+              slot="content"
+              key={v.jp + "-" + k}
+            >
+              <IonCheckbox
+                data-category={k}
+                data-value={v.jp}
+                onClick={changeCheckboxState}
+              />
+              <span>
+                {v.jp} - {v.pl.M}
+              </span>
             </div>
           ))}
         </IonAccordion>
       </IonAccordionGroup>
     );
   });
-  const b = nouns.animals.map((v) => (
-    <p>
-      <IonChip>
-        <IonCheckbox />
-        {v.jp}
-      </IonChip>
-    </p>
-  ));
 
   return (
     <div>
       <span>GrammarList</span>
       {a}
+      {' tu ma byc "a"'}
     </div>
   );
 }
---
+++ JpZd/src/components/JpQuestions.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function JpQuestions() {
+  return (
+    <div>JpQuestions</div>
+  )
+}
